7f6286027382906e20e21d8f807f1f3f8f191744
Amadi <whytes2256@gmail.com>
2023-06-12 06:54:40 -0700

    13-is_palindrome.c

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -17,6 +17,13 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+void reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
 def print_list_integer(my_list=[]);
 def element_at(my_list, idx);
 def replace_in_list(my_list, idx, element);
