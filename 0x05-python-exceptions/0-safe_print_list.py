db045c1a1a9551ac891d42b776394abc0c33675e
Amadi <whytes2256@gmail.com>
2023-06-26 16:37:20 -0700

    fixing code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0)
- "my_list contains intergers and strings
- ...elements must be printed on the same line followed by a new line
- ...x represents the number of elements to print
- ...x can be bigger than the length of my_list
-
+my_list=[]
+x = 0
