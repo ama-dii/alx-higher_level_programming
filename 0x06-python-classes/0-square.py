86fb967c39419be4f501eaf6357b7ace1cbb7428
Amadi <whytes2256@gmail.com>
2023-06-27 14:51:37 -0700

    fixing error

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-module that includes empty class
+	module that includes empty class
 """
 class Square:
-	"""
+"""
 	empty class Square that defines a square
-	"""
+"""
 	pass
