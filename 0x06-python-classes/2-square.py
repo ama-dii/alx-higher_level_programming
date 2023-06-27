1f8416ced6b24b6a84741f900ecb6582da40f6d1
Amadi <whytes2256@gmail.com>
2023-06-27 15:54:40 -0700

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module for class that defines a square"""
+
+class Square:
+    """A class that defines a square"""
+    def __init__(self, size=0):
+        """initializes the size"""
+        self.__size = size
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
