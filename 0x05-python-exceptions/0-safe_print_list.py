81eec7fb7462fd132f44d6efc47ad1fe04991657
Amadi <whytes2256@gmail.com>
2023-06-26 18:06:02 -0700

    fixing error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0)
-	count = 0
-	for element in my_list:
-		if count is >= x
-	print(element)
-	count =+ 1
-
+count = 0
+for i in range (x):
+	try:
+		print("{:d}".format(), end="")
+		count =+ 1
+	except IndexError:
+		break
+		print("")
 	return(count)
-
