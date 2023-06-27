d44840e89ac7b27ada224124c395bf7e8ac057e0
Amadi <whytes2256@gmail.com>
2023-06-26 17:23:21 -0700

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+	count = 0
+	for item in my_list
+		if isinstance(item, int):
+	print(item)
+	count += 1
+	"{:d}".format()
+	       return()
