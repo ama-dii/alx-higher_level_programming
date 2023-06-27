b13ac025f8ede33e2427d7a8580d352b29d735a7
Amadi <whytes2256@gmail.com>
2023-06-27 15:04:09 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" class module that defines a class"""
+class Square:
+	"""size is a private instance attribute"""
+	def __int__(self, size):
+	self.size = size
+	print(self.size)
