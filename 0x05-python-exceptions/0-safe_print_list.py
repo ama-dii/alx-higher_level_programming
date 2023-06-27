6fb1e393a68b2e68ece0217153e01c06f49055d3
Amadi <whytes2256@gmail.com>
2023-06-26 17:12:53 -0700

    fixing error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0)
- my_list=[x]
- x = 0
-print(my_list)
-	x =+ 1
-	return(x)
+	count = 0
+	for element in my_list:
+		if count is >= x
+	print(element)
+	count =+ 1
+
+	return(count)
 
