ac7c185af7adaac991fb998e91966e9693ae5c3b
Amadi <whytes2256@gmail.com>
2023-06-28 06:37:48 -0700

    adds code

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 #include "list.h"
 
-/** insert_node :
-*  @head : pointer to head of list
-*   @number:
+/** 
+* insert_node :
+* @head : pointer to head of list
+* @number:
+* Return: NULL
 ***/
 listint_t *insert_node(listint_t **head, int number)
